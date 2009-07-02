d0574a0abb7c224661492830f7a25a918fcf0d95
Steph Powell <steph@endpoint.com>
2009-07-02 18:36:33 -0400

    Added unit test for rating tied to review.

---
+++ test/extension_factories.rb
@@ -0,0 +1,7 @@
+Factory.define :review do |f|
+  f.content "This is awesome!"
+end
+
+#Factory.define :rating do |f|
+#	f.rating 3
+#end
--- test/unit/review_test.rb
+++ test/unit/review_test.rb
@@ -1,27 +1,27 @@
 require File.dirname(__FILE__) + '/../test_helper'
+#require File.dirname(__FILE__) + '/../extension_factories'
 require 'time'
 
 class ReviewTest < Test::Unit::TestCase
 	context 'user can only have one review per product' do
 		setup do
-			@product = Factory(:product, :name => 'Steph has a special product and this is it', :master_price => 0)
+			@product = Factory(:product)
 			@product.save
 			email = "foo#{Time.now.to_f.to_s}@test.com"
-			@user = Factory(:user, :login => email, :email => email,
-												:password => 'blah666', :password_confirmation => 'blah666')
+			@user = Factory(:user, :login => email, :email => email)
 			@user.save
 		end
 		should 'first review saved but second review fails' do
-			@review_one = Review.new(:title => 'This Product is Awesome',
-					:content => 'This Product is So Awesome. Thanks RailsDizzog.')
+			#@review_one = Factory(:review)
+			@review_one = Review.new(:content => 'This is awesome!')
 			@review_one.product = @product
 			@review_one.user = @user
-			@review_one.save!
+			@review_one.save
 			result = Review.find(:all, :conditions => {:product_id => @product.id, :user_id => @user.id})
 			assert_equal result.size, 1
 			assert_equal result[0].id, @review_one.id
-			@review_two = Review.new(:title => 'Awful',
-				:content => 'This product stinks like your code.')
+			#@review_two = Factory.review(:review)
+			@review_two = Review.new(:content => 'This is awful!')
 			@review_two.product = @product
 			@review_two.user = @user
 			begin
@@ -30,6 +30,27 @@ class ReviewTest < Test::Unit::TestCase
 				@exception = e
 			end
 			assert_not_nil @exception
+		end
+		should 'allow review to have and not have rating' do
+			#@review_one = Factory(:review)
+			@review_one = Review.new(:content => 'This is awesome!')
+			@review_one.product = @product
+			@review_one.user = @user
+			@review_one.save
+			assert_not_nil @review_one
+			assert_nil @review_one.rating
+			#@review_two = Factory(:review)
+			@review_two = Review.new(:content => 'This is awful!')
+			#@rating = Factory(:rating)
+			@rating = Rating.new(:rating => 3)
+			@rating.product = @product
+			@rating.user = @user
+			@review_two.rating = @rating
+			@review_two.save
+			@rating.save
+			assert_not_nil @review_two
+			assert_not_nil @rating
+			assert_equal @review_two.id, @rating.review_id
 		end
 	end
 end
